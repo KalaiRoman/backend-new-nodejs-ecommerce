1518f9eae1a42e4e39c47043622e7857424f3a40
kalai <kalai@cdp360.com>
2023-11-05 03:04:31 +0530

    remove node_modules

--- backend/dbconnect/Dbconnect.js
+++ backend/dbconnect/Dbconnect.js
@@ -4,7 +4,7 @@ const ConnectDb = async () => {
     try {
 
         mongoose.connect(process.env.MONGOOSEKEY).then((res) => {
-            console.log("DB Connected new");
+            console.log("DB Connected new wel");
         }).catch((err) => {
             console.log(err);
         })
