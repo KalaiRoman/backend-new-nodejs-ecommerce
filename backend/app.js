b570532eb2efce7b58d22cc6da848b26e61899a1
kalai <kalai@cdp360.com>
2023-11-05 01:19:02 +0530

    backend folders

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,18 @@
-console.log("welcome")
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import ConnectDb from './dbconnect/Dbconnect.js';
+dotenv.config();
+ConnectDb();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get("/kalai", (req, res, next) => {
+    res.send("welcome to kalai")
+})
+app.listen(process.env.PORT, () => {
+    console.log(`Port Running ${process.env.PORT}`)
+})
