784becbd9ccba67515b4ecaeb1d9c8610f88cc70
kalai <kalai@cdp360.com>
2023-11-05 15:04:59 +0530

    remove node_modules env

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import fs from 'fs';
+import { BSON } from 'bson';
 import ConnectDb from './dbconnect/Dbconnect.js';
 dotenv.config();
 ConnectDb();
@@ -9,12 +11,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const router = express.Router();
+
+router.get('/', (req, res) => res.render('welcome'));
 
 
 app.listen(process.env.PORT, () => {
     console.log(`Port Running ${process.env.PORT}`)
 })
 
-app.get("/kalai", (req, res) => {
-    res.send("welcome to kalai amma appa new thala")
-})
